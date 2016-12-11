8a7ead5b04dcc7c1e4056ee39d2e61c41d0eef07
AmunGurung <agurung.62045@farnborough.ac.uk>
2016-12-11 16:14:42 +0000

    Add files via upload

--- terms.java
+++ terms.java
@@ -28,7 +28,7 @@ public class terms
          System.out.println("3. End");
          System.out.println();
          System.out.println("Please key in choice: ");
-         
+         boolean found = false;
         
          do {
            choice = input.nextInt();
@@ -40,79 +40,37 @@ public class terms
          if (choice == one){
             System.out.println("Please enter the name of the term you wish to search: ");
             search = in.nextLine();    
-            
-             if (search.equals(terms[0])) {
-               System.out.println("Found. " + terms[1]); 
-             } else if (search.equals(terms[2])) {
-               System.out.println("Found. " + terms[3]);
-             } else if (search.equals(terms[4])) {
-               System.out.println("Found. " + terms[5]);
-             } else if (search.equals(terms[6])) {
-               System.out.println("Found. " + terms[7]);
-             } else if (search.equals(terms[8])) {
-               System.out.println("Found. " + terms[9]);
-             } else if (search.equals(terms[10])) {
-               System.out.println("Found. " + terms[11]);
-             } else if (search.equals(terms[12])) {
-               System.out.println("Found. " + terms[13]);
-             } else if (search.equals(terms[14])) {
-               System.out.println("Found. " + terms[15]);
-             } else if (search.equals(terms[16])) {
-               System.out.println("Found. " + terms[17]);
-             } else if (search.equals(terms[18])) {
-               System.out.println("Found. " + terms[19]);
-             } else   {
-               System.out.println("Term not found.");
-             }      
+            for (int i = 0 ; i<20 ; i++) {
+                  if (search.equals(terms[i])) {
+                       System.out.println("Found. " + terms[i+ 1]); 
+                       found = true;
+                } 
+              }  
+            if (found = (false)){
+                System.out.println("Term not found.");
+              }
             System.out.println();
          } 
          
-         
          Scanner last = new Scanner(System.in);
          String keyword;
+         boolean jack = false;
          if (choice == two) {
-            System.out.println("Please enter a keyboard: ");
-            keyword = last.nextLine();
-            
-               if (terms[1].contains(keyword))   {
-                   System.out.println("Found , for " + terms[0]);
-                   System.out.println(terms[1]);
-               } else if (terms[3].contains(keyword))   {
-                   System.out.println("Found , for " + terms[2]);
-                   System.out.println(terms[3]);
-               } else if(terms[5].contains(keyword))   {
-                   System.out.println("Found , for " + terms[4]);
-                   System.out.println(terms[5]);
-               } else if(terms[7].contains(keyword))   {
-                   System.out.println("Found , for " + terms[6]);
-                   System.out.println(terms[7]);
-               } else if(terms[9].contains(keyword))   {
-                   System.out.println("Found , for " + terms[8]);
-                   System.out.println(terms[9]);
-               } else if(terms[11].contains(keyword))   {
-                   System.out.println("Found , for " + terms[10]);
-                   System.out.println(terms[11]);
-               } else if(terms[13].contains(keyword))   {
-                   System.out.println("Found , for " + terms[12]);
-                   System.out.println(terms[13]);
-               } else if(terms[15].contains(keyword))   {
-                   System.out.println("Found , for " + terms[14]);
-                   System.out.println(terms[15]);
-               } else if(terms[17].contains(keyword))   {
-                   System.out.println("Found , for " + terms[16]);
-                   System.out.println(terms[17]);
-               } else if(terms[19].contains(keyword))   {
-                   System.out.println("Found , for " + terms[18]);
-                   System.out.println(terms[19]);
-               } else {
+            System.out.println("Please enter a keyword: ");
+            keyword = last.nextLine(); 
+            for (int i = 0 ; i<20 ; i+=2) {
+               if (terms[i+1].contains(keyword))   {
+                   System.out.println("Found , for " + terms[i]);
+                   System.out.println(terms[i+1]);
+                   jack = true;
+                }
+            } 
+            if (found = (false)){
                    System.out.println("No descriptions found containing this keyword.");
-               }
-                  
-               
+                }
             System.out.println();
             }
-         
-         
+            
     }   while (choice!=three);
   }   
 }
