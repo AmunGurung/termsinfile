81ed6f3fab057bfa3afb4d52327d950d14bc3cc4
AmunGurung <agurung.62045@farnborough.ac.uk>
2016-12-13 18:56:05 +0000

    Add files via upload

--- terms.java
+++ terms.java
@@ -6,71 +6,76 @@ import java.util.ArrayList;
 
 public class terms
 {
-     public static void main(String[] args) throws IOException
-     {
-       FileReader fr = new FileReader("C:\\Users\\User\\Desktop\\terms.txt");
-       BufferedReader br = new BufferedReader(fr);  
+    public static void main(String[] args) throws IOException
+    {
+        FileReader fr = new FileReader("U:\\Desktop_2k\\terms.txt");
+        BufferedReader br = new BufferedReader(fr);  
+
+        String str;
+        int choice;
         
-       String str;
-       int choice;
-       int three = 3;
-       int two = 2;
-       ArrayList<String> list = new ArrayList<String>();
-       while((str = br.readLine()) != null){
-              list.add(str);
-       }
-       String[] terms = list.toArray(new String[0]);
-       
-       do {
-         Scanner input = new Scanner(System.in);
-         System.out.println("1. Search for a term.");
-         System.out.println("2. Search for a keyword in the descriptions");
-         System.out.println("3. End");
-         System.out.println();
-         System.out.println("Please key in choice: ");
-         boolean found = false;
+        ArrayList<String> list = new ArrayList<String>();
+        while((str = br.readLine()) != null){
+            list.add(str);
+        }
+        String[] terms = list.toArray(new String[0]);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Search for a term.");
+        System.out.println("2. Search for a keyword in the descriptions");
+        System.out.println("3. End");
+        System.out.println();
+        System.out.println("Please key in choice: ");
         
-         do {
-           choice = input.nextInt();
-         }while (choice<1 | choice>3);
-    
-         Scanner in = new Scanner(System.in);
-         int one = 1;
-         String search = "n/a";
-         if (choice == one){
-            System.out.println("Please enter the name of the term you wish to search: ");
-            search = in.nextLine();    
-            for (int i = 0 ; i<20 ; i++) {
-                  if (search.equals(terms[i])) {
-                       System.out.println("Found. " + terms[i+ 1]); 
-                       found = true;
-                } 
-              }  
-            if (found = (false)){
-                System.out.println("Term not found.");
-              }
-            System.out.println();
-         } 
-         
-         Scanner last = new Scanner(System.in);
-         String keyword;
-         boolean jack = false;
-         if (choice == two) {
-            System.out.println("Please enter a keyword: ");
-            keyword = last.nextLine(); 
-            for (int i = 0 ; i<20 ; i+=2) {
-               if (terms[i+1].contains(keyword))   {
-                   System.out.println("Found , for " + terms[i]);
-                   System.out.println(terms[i+1]);
-                   jack = true;
-                }
-            } 
-            if (found = (false)){
-                   System.out.println("No descriptions found containing this keyword.");
-                }
-            System.out.println();
+        choice = input.nextInt();
+        do {
+            if (choice==1){
+                SearchbyTerm();
             }
-            
-    }   while (choice!=three);
-  }   
+            else if (choice==2){
+                SearchDescriptionsForKeyword();
+            }
+        } while (choice !=3);
+    }    
+        
+    public static void SearchbyTerm() throws IOException
+    {
+        Scanner in = new Scanner(System.in);
+        String search;
+        System.out.println("Please enter the name of the term you wish to search: ");
+        search = in.nextLine();    
+        boolean found = false;
+
+        for (int i = 0 ; i<20 ; i++) {
+            if (search.equals(terms[i])) {
+                    System.out.println("Found. " + terms[i+ 1]); 
+                    found = true;
+                } 
+         }  
+        if (found = false){
+                System.out.println("Term not found.");
+         }
+        System.out.println();
+        } 
+
+    public static void SearchDescriptionsForKeyword() throws IOException
+    {
+        Scanner last = new Scanner(System.in);
+        String keyword;
+        boolean found = false;
+        
+        System.out.println("Please enter a keyword: ");
+        keyword = last.nextLine(); 
+         for (int i = 0 ; i<20 ; i+=2) {
+           if (terms[i+1].contains(keyword))   {
+               System.out.println("Found , for " + terms[i]);
+               System.out.println(terms[i+1]);
+               found = true;
+           }
+        }    
+        if (found = false){
+            System.out.println("No descriptions found containing this keyword.");
+        }
+        System.out.println();
+    }
 }
